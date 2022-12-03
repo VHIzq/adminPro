3a5d9b9035c8af2095e0d4964d9657ae9fb7ec04
victor magdaleno <vrmq@gft.com>
2022-12-03 10:57:32 -0600

    add observable

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, retry } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -9,12 +9,32 @@ import { Observable } from 'rxjs';
 export class RxjsComponent {
 
   constructor() { 
+    let i = -1;
+
     const obs$ = new Observable(observer => {
-      setInterval(() => {
-        console.log('');
+
+      const interval = setInterval(() => {
+        i++; 
+        observer.next(i)
+        if (i === 5) {
+          clearInterval(interval);
+          observer.complete();
+        }
+
+        if (i === 2) {
+          i = 0;
+          observer.error('i llego al valor 2')
+        }
       }, 1000)
     });
-  }
 
+    obs$.pipe(
+      retry(1)
+    ).subscribe(
+      value => console.log('Subs', value), 
+      (err) => console.warn('Error', err),
+      () => console.info('Obs terminado')
+    )
+  }
 
 }
